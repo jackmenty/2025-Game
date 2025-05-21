2c3cfb14b35d9503e3e7ad4da56e40207c46ec92
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-22 10:23:19 +1200

    Fix y camera slowing down movement

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@onready var springarm = $SpringArm3D
+@onready var camera = $SpringArm3D/Camera3D
 
 
 func _physics_process(delta: float) -> void:
@@ -11,18 +13,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction: Vector3 = (springarm.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	if input_dir:
+		move_direction.y = 0
 
 	move_and_slide()

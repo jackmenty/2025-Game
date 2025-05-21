d87d2e29760f61810979ac007b521b46ec9e7448
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-21 12:44:15 +1200

    Added character

---
+++ Scenes/camera_spring_arm.gd
@@ -0,0 +1,24 @@
+extends SpringArm3D
+
+@export var mouse_sensitivity: float = 0.005
+@export_range(-90.0, 0.0, 0.1 , "radians_as_degrees") var min_vertical_angle: float = -PI/2
+@export_range(0.0, 90.0, 0.1 , "radians_as_degrees") var max_vertical_angle: float = PI/4
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x * mouse_sensitivity
+		rotation.y = wrapf(rotation.y ,0.0, TAU)
+		
+		rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.x = clamp(rotation.x , -PI/2, PI/4)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass

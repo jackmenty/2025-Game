013942609ba721d6926d3d2ea408de3928088a91
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-22 13:35:24 +1200

    fixed slow camera movement, needs to fix reverse camera movement

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -20,10 +20,11 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction: Vector3 = (springarm.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	print(springarm.transform.basis )
+	var corrected_dir = Vector3(direction.x, 0, direction.z).normalized()
+	if corrected_dir:
+		velocity.x = corrected_dir.x * SPEED
+		velocity.z = corrected_dir.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)

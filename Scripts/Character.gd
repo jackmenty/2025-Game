c502f770ef6e2f513a905d22a8e85cd0678b8914
jackmenty <140560202+jackmenty@users.noreply.github.com>
2025-05-26 14:32:04 +1200

    reverted back to regular speed version

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 1
+const SPEED = 20
 const MAX_SPEED = 10
 const JUMP_VELOCITY = 13.5
 const ACCELERATION = .5
@@ -22,19 +22,15 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (springarm.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction: Vector3 = (springarm.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	$RayCast3D.target_position=springarm.position
 	var corrected_dir = Vector3(direction.x, 0, direction.z).normalized()
 	if corrected_dir:
-		print(corrected_dir)
-		velocity.x += corrected_dir.x * SPEED
-		velocity.z += corrected_dir.z * SPEED
-		if (abs(velocity.x) >= MAX_SPEED):
-			velocity.x = MAX_SPEED * corrected_dir.x
-		if (abs(velocity.z) >= MAX_SPEED):
-			velocity.z = MAX_SPEED * corrected_dir.z
+		velocity.x = corrected_dir.x * SPEED
+		velocity.z = corrected_dir.z * SPEED
 	else:
-		velocity.x = velocity.x * delta * DECELERATION
-		velocity.z = velocity.z * delta * DECELERATION
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	print(velocity.x, " ",  velocity.z)
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)

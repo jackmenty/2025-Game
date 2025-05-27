d5b4c245a1c5011c9cf8497810d9d1c00f02fc05
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-27 12:43:38 +1200

    Added skybox and sprint

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -2,10 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 20
-const MAX_SPEED = 10
 const JUMP_VELOCITY = 13.5
-const ACCELERATION = .5
-const DECELERATION = 20
 @onready var springarm = $SpringArm3D
 @onready var camera = $SpringArm3D/Camera3D
 
@@ -23,15 +20,16 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction: Vector3 = (springarm.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	$RayCast3D.target_position=springarm.position
 	var corrected_dir = Vector3(direction.x, 0, direction.z).normalized()
 	if corrected_dir:
 		velocity.x = corrected_dir.x * SPEED
 		velocity.z = corrected_dir.z * SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x = corrected_dir.x * SPEED * 2
+			velocity.z = corrected_dir.z * SPEED * 2
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	print(velocity.x, " ",  velocity.z)
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		

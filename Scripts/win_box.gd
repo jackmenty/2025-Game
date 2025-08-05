2835804b48a2baf5c50b318764f903bfcfe480e1
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-08-06 11:44:57 +1200

    Start finishing the first level

--- Scripts/win_box.gd
+++ Scripts/win_box.gd
@@ -3,5 +3,5 @@ extends Node3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is CPlayer:
-		get_tree().change_scene_to_file("res://Scenes/Start_screen.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Sand_LVL.tscn")
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)

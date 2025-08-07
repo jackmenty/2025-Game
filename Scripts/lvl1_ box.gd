8468a4504af2feaaa4a491869947e5eb64c8c472
jackmenty <Rephrostar@gmail.com>
2025-08-08 00:07:17 +1200

    Export lvl1

---
+++ Scripts/lvl1_ box.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is CPlayer:
+		get_tree().change_scene_to_file("res://Scenes/Start_screen.tscn")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)

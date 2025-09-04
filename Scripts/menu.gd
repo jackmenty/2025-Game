a2e4fa47703e21b8dde85d8dbed637c6f3b5aa2c
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-09-05 10:24:56 +1200

    small update + bug patch (no important)

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -4,20 +4,20 @@ func _process(float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_parent().get_node("Menu").show()
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		Engine.time_scale = 0
+		get_tree().paused = true
 
 
 func _on_continue_pressed() -> void:
 	get_parent().get_node("Menu").hide()
+	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Engine.time_scale = 1
 
 
 func _on_return_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/Start_screen.tscn")
-	Engine.time_scale = 1
 
 
 func _on_reset_pressed() -> void:
+	get_tree().paused = false
 	get_tree().reload_current_scene()
-	Engine.time_scale = 1

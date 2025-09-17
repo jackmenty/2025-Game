64ed36bb10e088a1b441e813c461de1933b99a47
jackmenty <Rephrostar@gmail.com>
2025-09-17 19:58:09 +1200

    finished game for now

--- Scripts/lvl2_boxtscn.gd
+++ Scripts/lvl2_boxtscn.gd
@@ -1,8 +1,7 @@
 extends Node3D
 
 
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is CPlayer:
-		get_tree().change_scene_to_file("res://Scenes/Lava_LVL.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Water_LVL.tscn")
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
